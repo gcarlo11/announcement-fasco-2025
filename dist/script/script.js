b46e2be0208321e6df3c39639756b1a0959f61b3
gcarlo11 <ridhorasyid676@gmail.com>
2025-01-17 09:35:35 +0700

    a

--- dist/script/script.js
+++ dist/script/script.js
@@ -6,7 +6,7 @@ const salah = document.getElementById("salah");
 
 function handlePeriksa() {
   fetch("./dist/database/data.json")
-    .then((response) => response.json()) 
+    .then((response) => response.json())  
     .then((data) => {
       for (let i = 0; i < data.length; i++) {
         if (inputnim.value == data[i].nim) {
